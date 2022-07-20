fcf82e69b21e80e8c3e34b6be28792431d6c66fd
Beatriz Trindade <beatriztrindade@hotmail.com>
2022-07-20 20:26:29 -0300

    Projeto de Classes

---
+++ PetShop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using PetShop.SRC;
+
+
+namespace PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pets Gato = new Pets("Gato", "Felix", "Preto", "Angorá", "Macho");
+            Pets Cachorro = new Pets("Cachorro", "Spike", "Cinza", "Buldogue", "Macho");
+            Pets Peixe = new Pets("Peixe", "Cleo", "Laranja", "Dourado", "Fêmea");
+            Pets Passaro = new Pets("Passarinho", "Zé", "Verde", "papagaio", "Macho");
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("---> Pets para Adoção <--- ");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(Gato.ToString());
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(Cachorro.ToString());
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(Peixe.ToString());
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(Passaro.ToString());
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine(Gato.Cat());
+            Console.WriteLine(Cachorro.Dog());
+            Console.WriteLine(Peixe.Fish());
+            Console.WriteLine(Passaro.Bird());
+            Console.WriteLine("--------------------------");
+
+        }
+    }
+}
---
+++ PetShop/SRC/Pets.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.SRC
+{
+    internal class Pets
+    {
+        string Tipo { get; set; }
+        string Nome { get; set; }
+        string Cor { get; set; }
+        string Raca { get; set; }
+        string Sexo { get; set; }
+
+        public Pets(string tipo, string nome, string cor, string raca, string sexo)
+        {
+            Tipo = tipo;
+            Nome = nome;
+            Cor = cor;
+            Raca = raca;
+            Sexo = sexo;
+        }
+
+        public string Dog()
+        {
+            return $" O cachorro faz Auuu AuAuAuAuuuuu!!!";
+        }
+        public string Cat()
+        {
+            return $" O Gatinho faz Meowwwwww!!!!" ;
+        }
+        public string Fish()
+        {
+            return $" O Peixinho Gluub-Gluuub!!!";
+        }
+        public string Bird()
+        {
+            return $" O Passarinho faz Piu Pipiuuu!!!";
+        }
+
+        public override string ToString()
+        {
+            return $"Animal para adoção: {Tipo} \n" +
+                   $"Nome: {Nome} \n" +
+                   $"Cor: {Cor} \n" +
+                   $"Raça: {Raca} \n" +
+                   $"Sexo: {Sexo} \n";
+        }
+    }
+}
